1b0551e0040e06a1fe50ca0ca3b8c7d59dd51e57
Alex <skorohodov-ad@narod.ru>
2022-07-03 12:26:12 +0300

    Update scripts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-deploy_dir="/usr/bin"
+source os.sh
+
+if is_mac; then
+	deploy_dir="/usr/local/bin"
+else
+	deploy_dir="/usr/bin"
+fi
+
 cp Build-cmake/Release/nutrition_calculator "$deploy_dir"
 [ $? -ne 0 ] && echo " --- Errors during deploying at '$deploy_dir'" || echo " --- Deployed successfully at '$deploy_dir'"
