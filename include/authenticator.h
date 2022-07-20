da0be97d3d78c6f4279390d8a5eddc22e4839165
skalexey <skorohodov-ad@narod.ru>
2022-07-20 14:59:58 +0300

    Implementing authenticator

---
+++ include/authenticator.h
@@ -0,0 +1,37 @@
+﻿// authenticator.h
+
+#pragma once
+
+#include <chrono>
+#include <memory>
+#include <string>
+#include <filesystem>
+#include "http_client.h"
+
+namespace anp
+{
+	class authenticator : public http_client
+	{
+		using base = http_client;
+
+	public:
+		enum erc : int
+		{
+			parse_size_error = http_client::erc::count,
+			auth_error
+		};
+
+		int auth(
+			const std::string& host,
+			int port,
+			const std::string& user,
+			const std::string& token
+		);
+
+	protected:
+		void on_notify(int ec) override;
+		void on_reset() override;
+
+	private:
+	};
+}
---
+++ src/authenticator.cpp
@@ -0,0 +1,43 @@
+// authenticator.cpp
+
+#include <string>
+#include <algorithm>
+#include <type_traits>
+#ifdef __cpp_lib_format
+#include <format>
+#endif
+#include <fstream>
+#include <tcp/client.h>
+#include <utils/Log.h>
+#include <utils/string_utils.h>
+#include <utils/file_utils.h>
+#include <utils/datetime.h>
+#include <tcp/client.h>
+#include "authenticator.h"
+
+LOG_PREFIX("[authenticator]: ");
+LOG_POSTFIX("\n");
+SET_LOG_DEBUG(true);
+namespace fs = std::filesystem;
+namespace ch = std::chrono;
+
+namespace anp
+{
+	int authenticator::auth(
+		const std::string& host,
+		int port,
+		const std::string& user,
+		const std::string& token
+	)
+	{
+		return 0;
+	}
+
+	void authenticator::on_notify(int ec)
+	{
+	}
+
+	void authenticator::on_reset()
+	{
+	}
+}
