d048d200983eacc8c9532a7349f13b95f6e5fe9e
Alex <alexeiskorokhodov@gmail.com>
2022-09-02 20:37:35 +0300

    Update deps scenario.

--- deps_scenario.sh
+++ deps_scenario.sh
@@ -8,7 +8,7 @@ function deps_scenario()
     download_dependency "Utils" "$depsLocation" "git@github.com:skalexey/Utils.git"
     download_dependency "Networking" "$depsLocation" "git@github.com:skalexey/Networking.git"
     download_dependency "DataModelBuilder" "$depsLocation" "git@github.com:skalexey/DataModelBuilder.git"
-
+    source "$depsLocation/DataModelBuilder/Core/deps_scenario.sh"
     source $depsLocation/Networking/netlib/external_config.sh
 }
 
