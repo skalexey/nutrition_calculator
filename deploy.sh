c477ada34f10e7d44df5c5a450d8a1772dbbae01
Alex <alexeiskorokhodov@gmail.com>
2022-09-02 20:36:58 +0300

    Support deployment for Linux.

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,20 @@
 
 source os.sh
 
-if is_mac; then
-	deploy_dir="/usr/local/bin"
+if is_windows; then
+    deploy_dir="/usr/bin"
 else
-	deploy_dir="/usr/bin"
+    deploy_dir="/usr/local/bin"    	
 fi
 
-cp Build-cmake/Release/nutrition_calculator "$deploy_dir"
+f=Build-cmake/Release/nutrition_calculator
+[ ! -f "$f" ] && f=Build-cmake/nutrition_calculator
+[ ! -f "$f" ] && echo "No build found" && exit
+
+
+if is_linux; then
+    sudo cp $f "$deploy_dir"
+else
+    cp $f "$deploy_dir"
+fi
 [ $? -ne 0 ] && echo " --- Errors during deploying at '$deploy_dir'" || echo " --- Deployed successfully at '$deploy_dir'"
