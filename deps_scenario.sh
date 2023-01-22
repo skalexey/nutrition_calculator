7b373d5992b27f62aeb488ce7ede7cad408aa76c
skalexey <skorohodov-ad@narod.ru>
2023-01-22 22:25:11 +0100

    Auto update build scripts

--- deps_scenario.sh
+++ deps_scenario.sh
@@ -2,14 +2,15 @@
 
 function deps_scenario()
 {
-    source dependencies.sh
-    source deps_config.sh
+	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    source $THIS_DIR/dependencies.sh
+    source $THIS_DIR/deps_config.sh
 
     download_dependency "Utils" "$depsLocation" "git@github.com:skalexey/Utils.git"
     download_dependency "Networking" "$depsLocation" "git@github.com:skalexey/Networking.git"
     download_dependency "DataModelBuilder" "$depsLocation" "git@github.com:skalexey/DataModelBuilder.git"
     source "$depsLocation/DataModelBuilder/Core/deps_scenario.sh"
-    source $depsLocation/Networking/netlib/external_config.sh
+    source "$depsLocation/Networking/netlib/external_config.sh"
     download_dependency "php_include" "$depsLocation" "git@github.com:skalexey/php_include.git"
 }
 
