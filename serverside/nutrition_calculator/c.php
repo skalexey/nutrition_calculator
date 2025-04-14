97e5bb89ea24e7bf6ed122c4f7288d172fdaff9a
Alex <biggun@writeme.com>
2025-04-13 21:42:38 -0500

    update php config

--- serverside/nutrition_calculator/c.php
+++ serverside/nutrition_calculator/c.php
@@ -1,6 +1,7 @@
 <?php
 	$users_dir_name = "nc_u";
-	$db_name = "u834701438_nc";
-	$db_user = "u834701438_nc";
+	$db_name = "dbs14122449";
+	$db_user = "dbu336187";
 	$db_pass = "34.+[8-(Wn.M";
+	$db_host = "db5017656414.hosting-data.io";
 ?>
